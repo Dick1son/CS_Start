93ef4a05b9bac81d60b92f69c648271ba47f8090
Vladimir <vladimirtikhonov230@gmail.com>
2022-08-22 13:05:28 +0300

    Home work 2 first task added

--- Home_Works/HW_2/Program.cs
+++ Home_Works/HW_2/Program.cs
@@ -1,2 +1,12 @@
 ﻿
 //Task 1. Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+int SecondDigit(int num){
+    int result = num % 100 / 10;
+
+    return result; 
+}
+
+int randomNumber = new Random().Next(100, 1000);
+int completeNumber = SecondDigit(randomNumber);
+
+Console.Write($"Second digit of {randomNumber} is {completeNumber}.");
