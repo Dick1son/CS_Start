01a4183935cee53cc4db169b4a10f5945a00d25a
Vladimir <vladimirtikhonov230@gmail.com>
2022-08-25 11:31:36 +0300

    Example004 added

---
+++ Examples/Example004_Seminar_25August/Program.cs
@@ -0,0 +1,18 @@
+﻿int FindQuadrant ( double x, double y){
+    if (x > 0 && y > 0) return 1;
+    if (x < 0 && y > 0) return 2;
+    if (x < 0 && y < 0) return 3;
+    if (x > 0 && y < 0) return 4;
+    return 0;
+}
+
+Console.Write("Input X coordinate: ");
+double xA = Convert.ToDouble(Console.ReadLine());
+Console.Write("Input Y coordinate: ");
+double yA = Convert.ToDouble(Console.ReadLine());
+
+int quadrant = FindQuadrant(xA, yA);
+if (quadrant > 0)
+    Console.WriteLine($"Num of quad is {quadrant}");
+else
+    Console.WriteLine($"The point is on the axes");
